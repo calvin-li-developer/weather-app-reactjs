32a077f1596ac635d2320e8bde8bc43f1bb22983
calvin-li-developer <calvin.li.personal@gmail.com>
2023-12-29 10:30:04 -0500

    chore: improvement to app making more user friendly

--- src/App.js
+++ src/App.js
@@ -10,76 +10,84 @@ const api = {
 const DEBOUNCE_DELAY = 300;
 const TEMPERATURE_THRESHOLD = 16;
 
-function App() {
-  const [query, setQuery] = useState("");
+const App = () => {
+  const [query, setQuery] = useState('');
   const [weather, setWeather] = useState({});
-  const [defaultMessage, setdefaultMessage] = useState("Please Enter a City Name");
+  const [defaultMessage, setDefaultMessage] = useState('Please Enter a City Name');
+  const [loading, setLoading] = useState(false);
 
   const isValidCity = async (city) => {
     try {
-      // Fetch the Excel file from the URL
-      const response = await fetch(`${process.env.PUBLIC_URL}/assets/cities_list.xlsx`)
+      const response = await fetch(`${process.env.PUBLIC_URL}/assets/cities_list.xlsx`);
       const buffer = await response.arrayBuffer();
       const workbook = XLSX.read(new Uint8Array(buffer), { type: 'array' });
 
-      // Get the first sheet in the workbook
       const sheetName = workbook.SheetNames[0];
       const sheet = workbook.Sheets[sheetName];
 
-      // Convert the sheet to JSON
       const cityJSONData = XLSX.utils.sheet_to_json(sheet);
-
-      // Check if query in CityJSONData
-      const inCityJSONData = cityJSONData.some(obj => obj.name === city);
-
-      return inCityJSONData;
-    } catch (e) {
-      console.error('Error:', e);
+      return cityJSONData.some(obj => obj.name === city);
+    } catch (error) {
+      console.error('Error validating city:', error);
       return false;
     }
   };
 
-  const fetchWeather = debounce(async (query) => {
+  const fetchWeatherData = async (query) => {
     try {
       const response = await fetch(`${api.url}weather?q=${query}&units=metric&appid=${api.key}`);
       const result = await response.json();
+      setTimeout(() => {
+        console.log("Delayed for 1 second.");
+      }, 1000);      
       setWeather(result);
-    } catch (e) {
-      console.log('Error fetching weather data:', e.message);
+    } catch (error) {
+      console.error('Error fetching weather data:', error.message);
+      setDefaultMessage(`Error fetching weather data: ${error.message}`);
+    } finally {
+      setLoading(false);
     }
+  };
+
+  const fetchWeather = debounce(async (query) => {
+    setLoading(true);
+    await fetchWeatherData(query);
   }, DEBOUNCE_DELAY);
 
   const handleSearch = async (evt) => {
     if (evt.key === 'Enter') {
       if (query === '') {
-        setdefaultMessage("Please Enter a City Name");
+        setDefaultMessage('Please Enter a City Name');
         setWeather({});
+      } else {
+        const isValid = await isValidCity(query);
+        if (isValid) {
+          fetchWeather(query);
+        } else {
+          setDefaultMessage(`"${query}" city not found in database`);
+          setWeather({});
+        }
       }
-      else if (await isValidCity(query) === true) {
-        fetchWeather(query);
-      } 
-      else {
-        setdefaultMessage(`"${query}" city not found in database`);
-        setWeather({});
-      }
-      setQuery("");
+      setQuery('');
     }
   };
 
   const dateBuilder = useMemo(() => (d) => {
-    let months = ["January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December"]
-    let days = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"];
+    const months = ["January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December"];
+    const days = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"];
 
-    let day = days[d.getDay()];
-    let date = d.getDate();
-    let month = months[d.getMonth()];
-    let year = d.getFullYear();
+    const day = days[d.getDay()];
+    const date = d.getDate();
+    const month = months[d.getMonth()];
+    const year = d.getFullYear();
 
     return `${day} ${date}, ${month} ${year}`;
   }, []);
 
+  const weatherClass = weather.main && weather.main.temp > TEMPERATURE_THRESHOLD ? 'warm' : '';
+
   return (
-    <div className={`app ${weather.main && weather.main.temp > TEMPERATURE_THRESHOLD ? "warm" : ""}`}>
+    <div className={`app ${weatherClass}`}>
       <main>
         <div className="search-box">
           <input
@@ -91,7 +99,7 @@ function App() {
             onKeyPress={handleSearch}
           />
         </div>
-        {(typeof weather.main != "undefined") ? (
+        {(loading || weather.main) ? (
           <div>
             <div className="location-box">
               <div className="location">{weather.name}, {weather.sys.country}</div>
@@ -99,7 +107,7 @@ function App() {
             </div>
             <div className="weather-box">
               <div className="temp">
-                {Math.round(weather.main.temp)}°C
+                {loading ? 'Loading...' : `${Math.round(weather.main.temp)}°C`}
               </div>
               <div className="weather">{weather.weather[0].main}</div>
             </div>
